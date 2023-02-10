833f46818b59474bec369c3e530c3765686ea15c
LeeLoo3434 <lolarussell347@gmail.com>
2023-02-10 11:50:23 -0800

    model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Post(models.Model):
+    phrase = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    country_of_origin = models.CharField(max_length=200)
+    native_language = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.id}:{self.phrase[:10]}'
