cd9953cb307eb216e8fbfd9c291c46b73e5a372a
mariareeves <mariapaula.zimmers@gmail.com>
2023-02-14 14:48:41 -0700

    change template name and delete dead code

--- main_app/views.py
+++ main_app/views.py
@@ -23,9 +23,6 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
             context['profile_id'] = self.request.user.profile.id
-            print("Profile below ---------------------------------")
-            print(self.request.user.profile.id)
-            print(context['profile_id'])
         return context
 
     template_name = 'home.html'
