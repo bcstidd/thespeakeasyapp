079ed19a58e3193ce55a60d64aebc16e7b34ac57
cdgrody <cdgrody@gmail.com>
2023-02-15 09:50:12 -0800

    Remove comments from views.py

--- main_app/views.py
+++ main_app/views.py
@@ -9,11 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-
-
-# def home(request):
-#     return render(request, 'home.html')
 
 class HomeView(TemplateView):
     template_name = 'home.html'
