7a5f2153bac640e26580b888d2c98c1ec3dc03d1
cdgrody <cdgrody@gmail.com>
2023-02-15 11:29:40 -0800

    Form file has updates from profile form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['comment', 'language']
+
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
