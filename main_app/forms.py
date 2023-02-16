5891da6ce35172c58f07d9ba17890de8b6a8c2fc
cdgrody <cdgrody@gmail.com>
2023-02-16 15:50:10 -0800

    Modify view and forms file to add info to the add post form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Comment
+from .models import Comment, Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -17,3 +17,14 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['phrase', 'country_of_origin', 'native_language']
+        widgets = {
+            'phrase': forms.Textarea(attrs={'placeholder': 'Please enter your phrase here...'}),
+            'country_of_origin': forms.TextInput(attrs={'placeholder': 'Home Country'}),
+            'native_language': forms.TextInput(attrs={'placeholder': 'Native Language'}),
+        }
