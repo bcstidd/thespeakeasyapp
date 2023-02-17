cafb64b5ea5d24c4c45dbf84b5e9868e051ebac0
cdgrody <cdgrody@gmail.com>
2023-02-16 16:19:19 -0800

    Add and edit post pages styles are updated

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Comment, Post
+from .models import Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -17,14 +17,3 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
-
-
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['phrase', 'country_of_origin', 'native_language']
-        widgets = {
-            'phrase': forms.Textarea(attrs={'placeholder': 'Please enter your phrase here...'}),
-            'country_of_origin': forms.TextInput(attrs={'placeholder': 'Home Country'}),
-            'native_language': forms.TextInput(attrs={'placeholder': 'Native Language'}),
-        }
